c900fc3b151fa1145e3f616c9f4f0a0a2c4aa08e
hoonkim <hoonk9387@gmail.com>
2022-12-03 11:53:58 -0600

    Added jsx as variable.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+//   );
+
+// JSX - JavaScript XML
+// Babel transpiles JSX to JavaScript.
+const element = <h1 id="header" className="App">Eat your lunch first!!!</h1>; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  element
+  // <h1>Eat your fries!!!</h1>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
